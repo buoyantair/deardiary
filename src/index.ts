1b0bda283ed92f15ea5e13ec485467d3881921dd
buoyantair <buoyantair@protonmail.com>
2019-03-25 23:44:10 +0530

    feat: implement simple cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export default {}
+import * as program from 'commander'
+import { version } from '../package.json'
+
+program
+  .version(version)
+
+program
+  .command('ping')
+  .action(() => {
+    console.log('pong!')
+  })
+
+
+program.parse(process.argv);
+
