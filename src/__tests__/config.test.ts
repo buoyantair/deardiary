557118dbb1eb70f50300c97006e90df669210488
buoyantair <buoyantair@protonmail.com>
2019-03-27 12:29:48 +0530

    refactor: use caching mechanism to run tests

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -1,10 +1,16 @@
 import ConfigurationManager from "../config";
+const CACHE_PATH = `${__dirname}/cache`;
+const CONFIG_CACHE_PATH = `${CACHE_PATH}/config`;
+const DATA_CACHE_PATH = `${CACHE_PATH}/data`;
 
 describe("Configuration Management", () => {
   describe("getConfig()", () => {
-    test("getConfig should return a config object", async () => {
-      const result = await ConfigurationManager.getConfig();
-      expect(result).toEqual(ConfigurationManager.defaultConfig);
+    test("getConfig should return a config object with relevant properties", async () => {
+      const result = await ConfigurationManager.getConfig(
+        DATA_CACHE_PATH,
+        CONFIG_CACHE_PATH
+      );
+      expect(typeof result.diaryPath).toBe("string");
     });
   });
 
