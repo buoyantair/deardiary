6a4030ccdcf79e02537147e3e9613302ce90fec3
buoyantair <buoyantair@protonmail.com>
2019-03-27 12:37:02 +0530

    refactor: use better constants/globals

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -1,14 +1,16 @@
+import { constants as fsConstants, promises as fsPromises } from "fs";
 import ConfigurationManager from "../config";
 const CACHE_PATH = `${__dirname}/cache`;
-const CONFIG_CACHE_PATH = `${CACHE_PATH}/config`;
-const DATA_CACHE_PATH = `${CACHE_PATH}/data`;
+const CACHE_CONFIG_DIR_PATH = `${CACHE_PATH}/config`;
+const CACHE_DATA_DIR_PATH = `${CACHE_PATH}/data`;
+const CACHE_CONFIG_PATH = `${CACHE_CONFIG_DIR_PATH}/deardiary.settings`;
 
 describe("Configuration Management", () => {
   describe("getConfig()", () => {
     test("getConfig should return a config object with relevant properties", async () => {
       const result = await ConfigurationManager.getConfig(
-        DATA_CACHE_PATH,
-        CONFIG_CACHE_PATH
+        CACHE_DATA_DIR_PATH,
+        CACHE_CONFIG_DIR_PATH
       );
       expect(typeof result.diaryPath).toBe("string");
     });
