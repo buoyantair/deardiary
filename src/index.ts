d4f1324bbe204b00f10f4f118657ddc701586a2e
buoyantair <buoyantair@protonmail.com>
2019-04-06 17:54:00 +0530

    refactor: clear out code not used

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,6 @@ import * as program from "commander";
 import { version } from "../package.json";
 import configManager from "./config";
 
-const appendFile = promisify(fs.appendFile);
-
-configManager.getConfig().then(console.log);
-// (async () => {
-//   try {
-//     const test = await appendFile(
-//       "/home/flowerypastels/deardiary.settings",
-//       "Hello world"
-//     );
-//   } catch (e) {
-//     console.error(e);
-//   }
-// })();
-
 program.version(version);
 
 program.command("ping").action(() => {
